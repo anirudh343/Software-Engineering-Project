376e299c7356aadc7da8220a408ab0feb8d75d9d
Moinak Dey <moinakdey@ufl.edu>
2022-04-19 15:00:18 -0400

    Made Score Report dynamic

--- app/src/main/java/com/example/sweproject/ScienceQuiz.java
+++ app/src/main/java/com/example/sweproject/ScienceQuiz.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ScienceQuiz extends AppCompatActivity {
@@ -122,9 +127,6 @@ public class ScienceQuiz extends AppCompatActivity {
         String question10 = "10. " + returnList.get(9).getQuestion() + "\n" + q10w1 +"\n" + q10w2 +"\n" + q10w3 +"\n" + q10r + "\n";
 
         science10.setText(question10);
-
-
-
 
 
         //startActivity(new Intent(ScienceQuiz.this, studentscorepage.class));
